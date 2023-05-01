a1d625b06878f5e24a962833b4a3d858fee1cdc9
vitorueno <vitoruenootto@gmail.com>
2023-05-01 20:17:39 -0300

    feat: add autogenerated documentation with swagger

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const PORT = 3000;
+
 app.use(express.json());
+app.use(cors());
 
 // import routes
 const personRoute = require('./routes/person');
 
 // routes
-app.get('/', (req, res) => {
-    res.send('hello, world');
-});
-
 app.use('/person', personRoute);
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('../swagger_output.json');
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 
 // database 
 const mongoose = require('mongoose');
@@ -19,9 +24,10 @@ require('dotenv').config(); // database connection string, default port
 const connectToDatabase = async () => {
     try {
         const connection = await mongoose.connect(process.env.LOCAL_MONGO);
-        app.listen(process.env.PORT || 3000);
-        console.log('conectado');
+        app.listen(PORT);
+        console.log(`servidor conectado e rodando em http://localhost:${PORT}`);
     } catch (err) {
+        console.log(err);
         return err
     }
 };
--- src/routes/person.js
+++ src/routes/person.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { findOneAndDelete } = require('../models/Person');
 const router = express.Router();
 const Person = require('../models/Person');
 
