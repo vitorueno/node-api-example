a306bb4bb36adbd8a1d913fd8010057268e50411
vitorueno <vitoruenootto@gmail.com>
2023-05-03 18:14:55 -0300

    refactor: improve project legibility and add final details
    add comments in index.js to explain how to connect to database; improve swagger documentation; freezing dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const PORT = 3000;
 
+// middlewares
+const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
@@ -12,23 +12,30 @@ const personRoute = require('./routes/person');
 // routes
 app.use('/person', personRoute);
 
+// documentation route
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger_output.json');
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
-// database 
+// database connection
 const mongoose = require('mongoose');
-require('dotenv').config(); // database connection string, default port 
+
+// configuration file with database connection string, port... (not in repo) 
+require('dotenv').config();
+
+// default option if not using the .env file
+const DEFAULT_PORT = 3000; // default port
+const DEFAULT_CONNECTION_STRING = ''; // write your connection string here
 
 const connectToDatabase = async () => {
     try {
-        const connection = await mongoose.connect(process.env.LOCAL_MONGO);
-        app.listen(PORT);
-        console.log(`servidor conectado e rodando em http://localhost:${PORT}`);
+        const connection = await mongoose.connect(process.env.MONGO_CONNECTION_STRING || DEFAULT_CONNECTION_STRING);
+        app.listen(process.env.PORT || DEFAULT_PORT);
+        console.log(`servidor conectado e rodando em http://localhost:${DEFAULT_PORT}`);
     } catch (err) {
-        console.log(err);
-        return err
+        console.log('Falha ao conectar ao banco.');
+        console.log('Mensagem de erro: ', err);
     }
 };
 
--- src/routes/person.js
+++ src/routes/person.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const Person = require('../models/Person');
 
 router.get('/', async (req, res) => {
-    /* #swagger.description = 'retorna todas as pessoas.'
-       #swagger.tags = ['Person']
-    */
+    /* #swagger.description = 'retorna todas as pessoas cadastradas'
+       #swagger.tags = ['Person'] */
     try {
         const people = await Person.find();
         res.status(200).json(people);
@@ -16,15 +15,14 @@ router.get('/', async (req, res) => {
 
 
 router.post('/', async (req, res) => {
-    /*#swagger.description = 'modifica informação de uma pessoa específica'
+    /*#swagger.description = 'cadastra uma pessoa com os dados do corpo da requisição'
       #swagger.tags = ['Person']
       #swagger.parameters['obj'] = {
         in: 'body',
         description: 'Person information.',
         required: true,
         schema: { $ref: "#/definitions/Person" }
-      } 
-    */
+      } */
 
     const person = new Person({
         name: req.body.name,
@@ -43,8 +41,8 @@ router.post('/', async (req, res) => {
 
 
 router.get('/:personId', async (req, res) => {
-    // #swagger.description = 'pega informação de uma pessoa específica'
-    // #swagger.tags = ['Person']
+    /* #swagger.description = 'retorna os dados de uma pessoa específica (ID do Mongo)'
+    #swagger.tags = ['Person'] */
     try {
         const people = await Person.findById(req.params.personId);
         res.status(200).json(people);
@@ -54,8 +52,8 @@ router.get('/:personId', async (req, res) => {
 })
 
 router.delete('/:personId', async (req, res) => {
-    // #swagger.description = 'remove uma pessoa pelo ID do Mongo'
-    // #swagger.tags = ['Person']
+    /* #swagger.description = 'remove uma pessoa pelo ID do Mongo'
+     #swagger.tags = ['Person'] */
     try {
         const removedPerson = await Person.findOneAndDelete({ _id: req.params.personId });
         res.status(200).json(removedPerson);
@@ -66,14 +64,13 @@ router.delete('/:personId', async (req, res) => {
 
 router.put('/:personId', async (req, res) => {
     /* #swagger.description = 'substitui as informações da pessoa com ID informado pelos dados do corpo' 
-      #swagger.tags = ['Person']
-    #swagger.parameters['obj'] = {
+       #swagger.tags = ['Person']
+       #swagger.parameters['obj'] = {
             in: 'body',
             description: 'Person information.',
             required: true,
             schema: { $ref: "#/definitions/Person" }
-        } 
-    */
+        }  */
     try {
         const updatedPerson = await Person.findOneAndUpdate(req.params.personId, req.body);
         res.status(200).json(updatedPerson);
